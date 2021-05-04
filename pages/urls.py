d2837052c48ecd1ef162c55e67df3300853903f1
Sai SriHarsha Griddaluru <sriharsha.9554@gmail.com>
2021-05-04 20:26:26 +0530

    base-page-crated

--- pages/urls.py
+++ pages/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("about", views.about, name="about"),
+]

97104ac3944f5f64c4a97a47665f318a89c2fe2a
Sai SriHarsha Griddaluru <sriharsha.9554@gmail.com>
2021-05-05 03:51:22 +0530

    static-files-and-index-page

--- indivilla/settings.py
+++ indivilla/settings.py
@@ -118,7 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "indivilla/static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

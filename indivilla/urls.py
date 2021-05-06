b610eb588038817d2a7ef99200a0daae3d74d06e
Sai SriHarsha Griddaluru <sriharsha.9554@gmail.com>
2021-05-06 17:23:00 +0530

    listings-app-created-and-pages-linked

--- indivilla/urls.py
+++ indivilla/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("", include("pages.urls")),
+    path("listings", include("listings.urls")),
     path("admin/", admin.site.urls),
 ]

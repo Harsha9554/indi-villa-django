7f6afec863b143b20416363a884df69e1e0917fa
Sai SriHarsha Griddaluru <sriharsha.9554@gmail.com>
2021-05-06 20:10:33 +0530

    listings-model-and-agents-app-created

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from datetime import datetime
+from agents.models import Agent
 
-# Create your models here.
+
+class Listing(models.Model):
+    agent = models.ForeignKey(Agent, on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.IntegerField()
+    bedrooms = models.IntegerField()
+    bathrooms = models.IntegerField()
+    garage = models.IntegerField(default=0)
+    building_area = models.IntegerField()
+    photo_main = models.ImageField(upload_to="photos/%Y/%m/%d/")
+    photo_1 = models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True)
+    photo_2 = models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True)
+    photo_3 = models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True)
+    photo_4 = models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True)
+    photo_5 = models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True)
+    photo_6 = models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True)
+    is_published = models.BooleanField(default=True)
+    list_date = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.title

96bfdf2f912c74fa829a485a52e211e9a31631ba
Sai SriHarsha Griddaluru <sriharsha.9554@gmail.com>
2021-05-09 02:18:51 +0530

    loading-dynamic-data-from-db

--- listings/views.py
+++ listings/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from .models import Listing
 
 
 def index(request):
-    return render(request, "listings/listings.html")
+    listings = Listing.objects.filter(is_published=True)
+
+    context = {"listings": listings}
+
+    return render(request, "listings/listings.html", context)
 
 
 def listing(request):

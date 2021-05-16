0a4f9e7b02148a624faa170874845de9659e98fd
Sai SriHarsha Griddaluru <sriharsha.9554@gmail.com>
2021-05-16 20:12:58 +0530

    user-sign-up-and-login-authentication

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,50 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth, messages
 
 
 def signup(request):
-    return render(request, "accounts/signup.html")
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "that username already exists")
+            return redirect("signup")
+        elif User.objects.filter(email=email).exists():
+            messages.error(request, "that email already exists")
+            return redirect("signup")
+        else:
+            user = User.objects.create(username=username, email=email)
+            user.set_password(password)
+            user.save()
+            print("You are now registered and can log in")
+            messages.success(request, "You are now registered and can log in")
+            return redirect("login")
+    else:
+        return render(request, "accounts/signup.html")
 
 
 def login(request):
-    return render(request, "accounts/login.html")
+    if request.method == "POST":
+        print(request.POST)
+        username = request.POST["username"]
+        email = request.POST["username"]
+        password = request.POST["password"]
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, "You are now logged in")
+            return redirect("dashboard")
+        else:
+            messages.error(request, "Invalid credentials")
+            print("invalid")
+            return redirect("login")
+    else:
+        return render(request, "accounts/login.html")
 
 
 def logout(request):
